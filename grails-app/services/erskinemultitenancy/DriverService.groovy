0d4929b17aba87ca7242cd4371cf29bb201b7563
Tony Erskine <tony@sharptop.co>
2017-12-10 19:01:11 -0500

    added @CurrentTenant to DriverService

--- grails-app/services/erskinemultitenancy/DriverService.groovy
+++ grails-app/services/erskinemultitenancy/DriverService.groovy
@@ -1,7 +1,9 @@
 package erskinemultitenancy
 
+import grails.gorm.multitenancy.CurrentTenant
 import grails.gorm.services.Service
 
+@CurrentTenant
 @Service(Driver)
 interface DriverService {
     List<Driver> findAllDriversByStatus(DriverStatus status)
